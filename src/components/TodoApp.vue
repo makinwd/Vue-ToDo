f283a91bb98d8440dc4d67a8cdd0c63032863612
pustot <maki.nwd@gmail.com>
2022-12-12 00:06:22 +0300

    create to-do

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="container">
+    <h2 class="text-center mt-5">My Todo App</h2>
+    <!-- Input -->
+    <div class="d-flex">
+      <input
+        v-model="task"
+        type="text"
+        placeholder="Enter task"
+        class="form-control"
+      />
+      <button @click="submitTask" class="btn btn-warning rounded-0">
+        Submit
+      </button>
+    </div>
+    <!-- Task table -->
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col">Status</th>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>
+            <span :class="{ complete: task.status === 'complete' }">{{
+              task.name
+            }}</span>
+          </td>
+          <td style="width: 120px">
+            <span
+              @click="changeStatus(index)"
+              class="pointer"
+              :class="{
+                'text-danger': task.status === 'to-do',
+                'text-warning': task.status === 'in-progress',
+                'text-success': task.status === 'complete',
+              }"
+              >{{ firstcharUpper(task.status) }}</span
+            >
+          </td>
+          <td>
+            <div class="text-center" @click="editTask(index)">
+              <span class="fa fa-pen"></span>
+            </div>
+          </td>
+          <td>
+            <div class="text-center" @click="deleteTask(index)">
+              <span class="fa fa-trash"></span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoApp',
+  props: {
+    msg: String,
+  },
+
+  data() {
+    return {
+      task: '',
+      editedTask: null,
+      availableStatuses: ['to-do', 'in-progress', 'complete'],
+      tasks: [
+        {
+          name: 'Learn Vue and Vite',
+          status: 'to-do',
+        },
+        {
+          name: 'Write Todo with React & redux',
+          status: 'in-progress',
+        },
+      ],
+    };
+  },
+
+  methods: {
+    submitTask() {
+      if (this.task.length === 0) return;
+
+      if (this.editTask === null) {
+        this.tasks.push({ name: this.task, status: 'to-do' });
+      } else {
+        this.tasks[this.editedTask].name = this.task;
+        this.editedTask = null;
+      }
+      this.task = '';
+    },
+
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+
+    editTask(index) {
+      this.task = this.tasks[index].name;
+      this.editedTask = index;
+    },
+
+    changeStatus(index) {
+      let newIndex = this.availableStatuses.indexOf(this.tasks[index].status);
+      if (++newIndex > 2) newIndex = 0;
+      this.tasks[index].status = this.availableStatuses[newIndex];
+    },
+
+    firstcharUpper(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.pointer {
+  cursor: pointer;
+}
+.complete {
+  text-decoration: line-through;
+}
+</style>
